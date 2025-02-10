f879482b4b931394cf09a1f8492062443b670537
Prio <prio96@icloud.com>
2025-02-10 12:01:37 +0600

    member and staff model created

--- member/models.py
+++ member/models.py
@@ -1,7 +1,16 @@
 from django.db import models
 from django.contrib.auth.models import User
+
 # Create your models here.
 class MemberModel(models.Model):
     user=models.OneToOneField(User,related_name="member",on_delete=models.CASCADE)
+    image=models.ImageField(upload_to="member/images/")
+    mobile_no=models.CharField(max_length=12)
+    weight=models.FloatField(help_text="Weight in kilograms")
+    height=models.FloatField(help_text="Height in centimeters")
+    
+    def __str__(self):
+        return f"{self.user.first_name} {self.user.last_name}"
+    
     
 
--- subscription/models.py
+++ subscription/models.py
@@ -1,3 +1,19 @@
 from django.db import models
-
+from member.models import MemberModel
 # Create your models here.
+class MembershipPlanModel(models.Model):
+    PLAN_CHOICES=[
+        ('Weekly', 'Weekly'),
+        ('Monthly', 'Monthly'),
+        ('Yearly', 'Yearly')
+    ]
+    name=models.CharField(max_length=50)
+    description=models.TextField()
+    price=models.DecimalField(max_digits=8, decimal_places=2)
+    duration=models.CharField(max_length=10, choices=PLAN_CHOICES)
+
+    def __str__(self):
+        return self.name
+
+class MemberSubscriptionModel(models.Model):
+    member=models.ForeignKey(MemberModel, )
