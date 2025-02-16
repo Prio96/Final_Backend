d93f39008a4b18bb31640f411574db7d14f29ecd
Prio <prio96@icloud.com>
2025-02-16 19:10:37 +0600

    Change 3

--- GMS_Backend/settings.py
+++ GMS_Backend/settings.py
@@ -37,7 +37,10 @@ DEBUG = True
 
 ALLOWED_HOSTS = ['127.0.0.1','.vercel.app']
 
-CORS_ALLOW_ALL_ORIGINS = True
+CORS_ALLOWED_ORIGINS = [
+    "http://127.0.0.1:3000",
+    "http://localhost:3000"
+]
 
 CORS_ALLOW_CREDENTIALS = True
 
